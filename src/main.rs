d127a7199cf58d2a1560cca9bddc1ecd340fd2e2
Alexander Jiang <dashuai99@gmail.com>
2023-05-01 11:55:16 -0700

    refactor the data struct for a hand to be a map from tile_suit to a map from tile_rank to count in hand, new unit test

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,24 @@
 
 use std::collections::HashMap;
 
-fn check_valid_tile_string(tile_str: &String) {
-    assert!(tile_str.len() == 2);
-    assert!(
-        tile_str.starts_with('1')
-            || tile_str.starts_with('2')
-            || tile_str.starts_with('3')
-            || tile_str.starts_with('4')
-            || tile_str.starts_with('5')
-            || tile_str.starts_with('6')
-            || tile_str.starts_with('7')
-            || tile_str.starts_with('8')
-            || tile_str.starts_with('9')
-            || tile_str.starts_with('E')
-            || tile_str.starts_with('S')
-            || tile_str.starts_with('W')
-            || tile_str.starts_with('N')
-            || tile_str.starts_with('G')
-            || tile_str.starts_with('R')
-    );
-    assert!(
-        tile_str.ends_with('m')
-            || tile_str.ends_with('p')
-            || tile_str.ends_with('s')
-            || tile_str.ends_with('w')
-            || tile_str.ends_with('d')
-    );
-}
+// const NUMBER_TILES: [&str; 9] = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// const WIND_TILES: [&str; 4] = ["E", "S", "W", "N"];
+// const DRAGON_TILES: [&str; 3] = ["G", "R", "W"];
+// const TILE_SUITS: [&str; 5] = ["m", "p", "s", "w", "d"];
+const HONOR_SUITS: [&str; 2] = ["w", "d"];
+// const NUMBER_SUITS: [&str; 3] = ["m", "p", "s"];
 
-fn is_honor_tile(tile_str: &String) -> bool {
-    check_valid_tile_string(tile_str);
-    tile_str.ends_with('w') || tile_str.ends_with('d')
-}
+// fn check_valid_tile_string(tile_str: &String) {
+//     assert!(tile_str.len() == 2);
+//     let tile_rank = tile_str.get(0..1).expect("Expected tile rank");
+//     let tile_suit = tile_str.get(1..2).expect("Expected tile suit");
+//     assert!(
+//         NUMBER_TILES.contains(&tile_rank)
+//             || WIND_TILES.contains(&tile_rank)
+//             || DRAGON_TILES.contains(&tile_rank)
+//     );
+//     assert!(TILE_SUITS.contains(&tile_suit));
+// }
 
 fn is_winning_hand(tiles: Vec<String>) -> bool {
     // check hand length, must be minimum 14 tiles (could be more if there are quads)
@@ -42,20 +28,65 @@ fn is_winning_hand(tiles: Vec<String>) -> bool {
         return false;
     }
 
-    // build frequency count mapping
-    let mut tile_counts = HashMap::new();
+    // TODO check for edge case hands: 7 pairs and 13 orphans
+
+    // build frequency count per tile suit: mapping of tile suit -> (mapping of rank -> count)
+    let mut tile_counts_by_suit: HashMap<String, HashMap<String, i32>> = HashMap::new();
     for tile_str in tiles {
-        let count = tile_counts.entry(tile_str).or_insert(0);
-        *count += 1;
+        let tile_rank = String::from(tile_str.get(0..1).expect("Expected tile rank"));
+        let tile_suit = String::from(tile_str.get(1..2).expect("Expected tile suit"));
+
+        if tile_counts_by_suit.contains_key(&tile_suit) {
+            let suit_counts = tile_counts_by_suit.get_mut(&tile_suit).unwrap();
+            let count = suit_counts.entry(tile_rank).or_insert(0);
+            *count += 1;
+        } else {
+            let mut empty_counts = HashMap::new();
+            // we have to update/mutate `empty_counts` before we call `tile_counts_by_suit.insert(...)`,
+            //  as that insert will transfer ownership of `empty_counts`
+            empty_counts.insert(tile_rank, 1);
+            tile_counts_by_suit.insert(tile_suit, empty_counts);
+        }
     }
-    println!("{:?}", tile_counts);
+    println!("{:?}", tile_counts_by_suit);
 
-    // TODO check honors
-    // any isolated honors? if so, not winning
-    // if there is a pair, that must be the only pair in the handsrc/
+    // there can be at most one pair
+    let mut pair_tile = String::from("");
 
-    // TODO then check numbered suits
-    // start from the lowest number in the suit
+    // check honor tiles:
+    // - any isolated honors? if so, not winning
+    // - if there is a pair, that must be the only pair in the hand
+    for (tile_suit, suit_counts) in tile_counts_by_suit {
+        if !HONOR_SUITS.contains(&(tile_suit.as_str())) {
+            continue;
+        }
+        for (tile_rank, tile_count) in suit_counts {
+            let mut new_tile_str = String::new();
+            new_tile_str.push_str(tile_rank.as_str());
+            new_tile_str.push_str(tile_suit.as_str());
+
+            if tile_count == 1 {
+                // isolated honor tile
+                println!("isolated honor tile {new_tile_str}");
+                return false;
+            } else if tile_count == 2 {
+                if !pair_tile.is_empty() {
+                    // honor tile must be the pair, but can only have one pair in the winning hand
+                    println!("pair of honor tile {new_tile_str} but already have a pair");
+                    return false;
+                } else {
+                    pair_tile = new_tile_str;
+                }
+            }
+        }
+    }
+
+    println!("pair tile so far = {}", pair_tile);
+
+    // TODO check numbered suits:
+    // - start from the lowest number in the suit
+    // - there must be at least 2 tiles of that number, or a tile of the next higher number
+    // -
 
     return false;
 }
@@ -63,10 +94,6 @@ fn is_winning_hand(tiles: Vec<String>) -> bool {
 fn main() {
     // let tile = Tile { suit: TileSuit::Man, rank: NumberTileType::Five};
     // println!("It's a tile: {}", tile.repr());
-    let tile = String::from("4s");
-    println!("is 4s honor tile? {}", is_honor_tile(&tile));
-    let wind_tile = String::from("Ww");
-    println!("is WW honor tile? {}", is_honor_tile(&wind_tile));
 
     let mut hand: Vec<String> = Vec::new();
     hand.push(String::from("1s"));
@@ -94,6 +121,34 @@ fn main() {
 mod tests {
     // importing names from outer (for mod tests) scope.
     use super::*;
+
+    #[test]
+    fn test_is_winning_hand_half_flush() {
+        // from: https://riichi.wiki/Honiisou
+        let tiles = Vec::from([
+            String::from("1m"),
+            String::from("1m"),
+            String::from("1m"),
+            String::from("2m"),
+            String::from("3m"),
+            String::from("4m"),
+            String::from("8m"),
+            String::from("8m"),
+            String::from("Gd"),
+            String::from("Gd"),
+            String::from("Ww"),
+            String::from("Ww"),
+            String::from("Ww"),
+        ]);
+
+        for winning_tile in [String::from("8m"), String::from("Gd")] {
+            let mut new_tiles = Vec::new();
+            new_tiles.clone_from_slice(tiles.as_slice());
+            new_tiles.push(winning_tile);
+            assert_eq!(is_winning_hand(new_tiles), true);
+        }
+        assert_eq!(is_winning_hand(tiles), false);
+    }
 
     #[test]
     fn test_is_winning_hand_isolated_honor() {
